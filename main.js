d384e1af7e3b5c4299005381f81b10faab2993a7
Juan Valencia <valenciacamilo5926@gmail.com>
2025-02-22 15:41:51 -0500

    Se hizo una corrección en la funcion generarColores para que diera todos los numeros del hexadecimal

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 let colores = [];
-colores = Array.from(document.querySelectorAll('.color'));
-generar = document.querySelector('button');
-generar.addEventListener('click', posicionarColor);
+colores = Array.from(document.querySelectorAll(".color"));
+generar = document.querySelector("button");
+generar.addEventListener("click", posicionarColor); //Para asignar el evento al hacerle click al boton
 
+//Llama a la funcion posicionarColor para que se ejecute al cargar la pagina
 posicionarColor();
 
+//Funcion que llama a la funcion generarColores y asi asignar el color resultante a el texto y al color de fondo
 function posicionarColor() {
   colores.forEach((color) => {
     let colorGenerado = `#${generarColores()}`;
@@ -13,12 +15,34 @@ function posicionarColor() {
   });
 }
 
-//Funcion que genera un numeros aleatorios para generar colores hexadecimales 
-function generarColores(){
-  let color = '';
+//Funcion que genera un numeros aleatorios para generar colores hexadecimales
+function generarColores() {
+  let color = "";
   for (let i = 0; i < 6; i++) {
-    let numero = Math.floor(Math.random()* 10);
+    let numero = Math.floor(Math.random() * 16);
+    if (numero > 9) {
+      switch (numero) {
+        case 10:
+          numero = "A";
+          break;
+        case 11:
+          numero = "B";
+          break;
+        case 12:
+          numero = "C";
+          break;
+        case 13:
+          numero = "D";
+          break;
+        case 14:
+          numero = "E";
+          break;
+        case 15:
+          numero = "F";
+          break;
+      }
+    }
     color += String(numero);
   }
-  return color
+  return color;
 }
